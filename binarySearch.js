2b4d151b8203b7fb559e1132bfbb776805ddbe3a
Pedro Pablo Rodriguez <pedro@hazcerveza.com>
2017-02-25 11:55:11 -0600

    pivot fixed

--- binarySearch.js
+++ binarySearch.js
@@ -1,19 +1,19 @@
 var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,50];
-var searchTerm = 31;
+var searchTerm = 3;
 
-function binarySearch (arr, searchTerm){ 
+function binarySearch (arr, searchTerm){
 	var pivotIndex;
 
 	if(arr.length <= 1 && arr[0] !== searchTerm){
 		return false;
 	}
-	
+
 	pivotIndex = Math.floor(arr.length/2);
-	
-	if(arr[pivotIndex] === searchTerm || arr[pivotIndex-1] === searchTerm){
+
+	if(arr[pivotIndex] === searchTerm){
 		return true;
 	}else if(arr[pivotIndex] < searchTerm){
-		return binarySearch(arr.slice(pivotIndex, arr.length), searchTerm);
+		return binarySearch(arr.slice(pivotIndex + 1, arr.length), searchTerm);
 	}else{
 		return binarySearch(arr.slice(0, pivotIndex), searchTerm);
 	}
